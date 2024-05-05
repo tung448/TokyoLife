af489241730c8da5b76922fbd82bcf97a3deb7a7
unknown <tung18883@gmail.com>
2024-05-05 10:34:29 +0700

    Boot Trap 4

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const handlebar = require('express-handlebars');
+const path = require('path');
+
+
+
+const app = express();
+const port = 3000
+
+app.use(express.static(path.join(__dirname,'public'))); // static file
+
+
+
+//temple engine
+app.engine('hbs', handlebar.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');  
+app.set('views', path.join(__dirname, 'resources/views')); // unstable directory of handlebars
+
+console.log(path.join(__dirname, 'resources/views'));
+
+//home
+app.get('/', (req,res)=>{ // req client // res from server
+    res.render('home'); // render home.handlebars
+});
+
+// news
+app.get('/news', (req,res)=>{ // req client // res from server
+    res.render('news'); // render home.handlebars
+});
+
+
+app.listen(port,()=>console.log('App listening at local host'));
+
